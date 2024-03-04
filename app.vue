a732cb3725d3bba831b84ddf04ac7826672fa17c
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-05 00:16:33 +0100

    jobs info

---
+++ app.vue
@@ -0,0 +1,34 @@
+<script>
+import { defineNuxtComponent } from "#app";
+import axios from "axios";
+
+export default defineNuxtComponent({
+	data: () => ({
+		jobs: [],
+	}),
+	methods: {
+		async fetchData() {
+			const response = await axios.get(
+				"https://edwardtanguay.vercel.app/share/jobs.json"
+			);
+			this.jobs = response.data;
+		},
+	},
+});
+</script>
+<template>
+	<div>
+		<h1>Info Site</h1>
+		<img src="/jobs.png" />
+		<button @click="fetchData">fetch data</button>
+		<ul>
+			<li v-for="job in jobs" :key="job.id">
+				<input
+					type="checkbox"
+					:checked="job.skillList.includes('react')"
+				/>
+				{{ job.title }}
+			</li>
+		</ul>
+	</div>
+</template>
---
+++ interfaces.ts
@@ -0,0 +1,9 @@
+export interface IJob {
+	id: number;
+	title: string;
+	company: string;
+	url: string;
+	description: string;
+	skillList: string;
+	publicationDate: string;
+}
