1fb583970eed68c6c62ce82ee8dd49cc7c9ebd4b
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-05 07:55:35 +0100

    check in

--- app.vue
+++ app.vue
@@ -6,6 +6,11 @@ export default defineNuxtComponent({
 	data: () => ({
 		jobs: [],
 	}),
+	computed: {
+		numberOfReactJobs() {
+			return this.jobs.filter(m => m.skillList.includes('react')).length;
+		}
+	},
 	methods: {
 		async fetchData() {
 			const response = await axios.get(
@@ -22,7 +27,7 @@ export default defineNuxtComponent({
 		<h1 class="text-3xl mb-3">Info Site</h1>
 		<img src="/jobs.png" class="w-[24rem] rounded-lg mb-3" />
 		<button @click="fetchData" class="mb-3">fetch data</button>
-		<h2 class="text-xl mb-3" v-if="jobs.length > 0">checked = React</h2>
+		<h2 class="text-xl mb-3" v-if="jobs.length > 0">checked = React ({{ numberOfReactJobs }})</h2>
 		<ul>
 			<li v-for="job in jobs" :key="job.id">
 				<input
