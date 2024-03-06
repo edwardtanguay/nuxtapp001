60b5f4354c860ac9a54301c1a2de7062c38c04cc
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-06 08:11:14 +0100

    jobs component works

--- app.vue
+++ app.vue
@@ -1,32 +1,12 @@
 <script setup>
-import axios from "axios";
 import Jobs from './components/Jobs';
-
-let jobs = ref([]);
-
-const numberOfReactJobs = computed(() => {
-	return jobs.value.filter((m) => m.skillList.includes("react")).length;
-});
-
-const fetchData = async () => {
-	const response = await axios.get(
-		"https://edwardtanguay.vercel.app/share/jobs.json"
-	);
-	jobs.value = response.data;
-};
 </script>
 
 <template>
 	<div>
 		<h1 class="text-3xl mb-3">Info Site</h1>
 		<img src="/jobs.png" class="w-[24rem] rounded-lg" />
-		<button @click="fetchData" class="mb-3">fetch data</button>
-		<h2 class="text-xl mb-3" v-if="jobs.length > 0">
-			checked = React ({{ numberOfReactJobs }})
-		</h2>
-		<ul class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2">
-			<Jobs :jobs="jobs"/>
-		</ul>
+		<Jobs/>
 	</div>
 </template>
 
--- components/Jobs.vue
+++ components/Jobs.vue
@@ -1,18 +1,30 @@
 <script setup>
-const props = defineProps({
-	jobs: {
-		type: Array,
-		required: true,
-	},
+import axios from "axios";
+let jobs = ref([]);
+
+const numberOfReactJobs = computed(() => {
+	return jobs.value.filter((m) => m.skillList.includes("react")).length;
 });
-const { jobs } = props;
+
+const fetchData = async () => {
+	const response = await axios.get(
+		"https://edwardtanguay.vercel.app/share/jobs.json"
+	);
+	jobs.value = response.data;
+};
 </script>
 
 <template>
-	<li v-for="job in jobs" :key="job.id">
-		<input type="checkbox" :checked="job.skillList.includes('react')" />
-		{{ job.title }}
-	</li>
+	<button @click="fetchData" class="mb-3">fetch data</button>
+	<h2 class="text-xl mb-3" v-if="jobs.length > 0">
+		checked = React ({{ numberOfReactJobs }})
+	</h2>
+	<ul class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2">
+		<li v-for="job in jobs" :key="job.id">
+			<input type="checkbox" :checked="job.skillList.includes('react')" />
+			{{ job.title }}
+		</li>
+	</ul>
 </template>
 
 <style scoped></style>
