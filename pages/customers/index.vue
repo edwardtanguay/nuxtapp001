e3b8258903911e579c041024b0f036850dc14104
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-08 20:54:47 +0100

    loading not working

--- pages/customers/index.vue
+++ pages/customers/index.vue
@@ -1,21 +1,32 @@
 <script setup>
-const { data: products } = await useFetch("https://fakestoreapi.com/products");
+const { pending, data: products } = await useFetch(
+	"https://fakestoreapi.com/products",
+	{
+		lazy: false,
+	}
+);
 </script>
 
 <template>
-	<h2 class="text-xl mb-3 mt-6">{{ products.length }} products</h2>
-	<ul>
-		<li
-			v-for="product in products"
-			class="flex gap-3 items-center mb-3 text-xl"
-		>
-			<img
-				class="w-[3rem] rounded border-4 border-white"
-				:src="product.image"
-			/>
-			<div>
-				{{ product.title }}
-			</div>
-		</li>
-	</ul>
+	<div>{{ pending }}</div>
+	<div v-if="pending">
+		<p>Loading...</p>
+	</div>
+	<div v-else>
+		<h2 class="text-xl mb-3 mt-6">{{ products.length }} products</h2>
+		<ul>
+			<li
+				v-for="product in products"
+				class="flex gap-3 items-center mb-3 text-xl"
+			>
+				<img
+					class="w-[3rem] rounded border-4 border-white"
+					:src="product.image"
+				/>
+				<div>
+					{{ product.title }}
+				</div>
+			</li>
+		</ul>
+	</div>
 </template>
